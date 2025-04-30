90bc23313b3c3ab42c64de035634947064655790
Rodny Estrada <rrodnyestrada1@gmail.com>
2025-04-30 00:40:16 -0400

    feat: downgrade tailwind version

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{css,html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        serrat: 'var(--font-serrat)',
+      },
+      colors: {
+        body: 'var(--color-body)',
+        primary: 'var(--color-primary)',
+        'primary-content': 'var(--color-primary-content)',
+        'body-content': 'var(--color-body-content)',
+      },
+    },
+  },
+  plugins: [],
+};
